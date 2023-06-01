d7fa3f294bbdb2a99ca478827959cc355b533c49
Devfrank94 <francescomurro94@gmail.com>
2023-06-01 16:55:17 +0200

    add Hello World

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Primi Passi</title>
+</head>
+
+<style>
+    h1{
+    text-align: center;
+    color: white;
+    font-size: 3rem;
+    }
+
+    body{
+    background-image: linear-gradient(90deg, #a1c4fd 0%, #c2e9fb 100%);
+
+    }
+
+</style>
+
+<body>
+
+    <h1>Hello World</h1>
+
+
+
+</body>
+</html>
